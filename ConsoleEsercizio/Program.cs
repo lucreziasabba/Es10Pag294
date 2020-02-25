1d9487ce07a65dbac3eda81a79d8c42e1b0b3af6
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-02-25 08:55:56 +0100

    Add project files.

---
+++ ConsoleEsercizio/Program.cs
@@ -0,0 +1,36 @@
+﻿using LibreriaEsercizio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleEsercizio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int TANTI = 10;
+            int[] array = new int[TANTI];
+            Random r = new Random();
+            List<int> numeri = new List<int>();
+            for (int i = 0; i < TANTI; i++)
+            {
+                int k;
+                do
+                {
+                    k = r.Next(0, 100);
+                } while (numeri.Contains(k));
+                numeri.Add(k);
+                array[i] = k;
+            }
+            int[] sortedArray = Classe.Vett(array);
+            for (int i = 0; i < TANTI; i++)
+            {
+                Console.Write(sortedArray[i] + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LibreriaEsercizio/Classe.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibreriaEsercizio
+{
+    public class Classe
+    {
+        public static int[] Vett(int[]array)
+        {
+            int[] minori = new int[array.Length];
+            int[] array_ordinato = new int[array.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                int c = 0;
+                for (int j = 0; j < array.Length; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        c++;
+                    }
+                }
+                minori[i] = c;
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                array_ordinato[minori[i]] = array[i];
+            }
+            return array_ordinato;
+        }
+    }
+}
